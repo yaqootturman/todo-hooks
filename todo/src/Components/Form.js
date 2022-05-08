fb6be9f1b4104d14c0ab792848c8e58c2a9230d8
yaqootturman <yaqoot.turman@gmail.com>
2022-05-08 21:42:22 +0300

    learn useEffect hooks

--- todo/src/Components/Form.js
+++ todo/src/Components/Form.js
@@ -1,12 +1,29 @@
 import './style.css'
+import {useState,useEffect} from 'react'
+import Task from './Task'
+
 const Form = ()=>{
+    const [task, setTask] = useState('') 
+    const [tasks, setArray] = useState([])
+
+    useEffect(() => {
+        console.log(':UseEfferct');
+        <Task/>
+    },[tasks]);
+      
     return(
         <>
             <form className="form" onSubmit={(e)=>{
                 e.preventDefault()
-
+                setArray(prevTask => [...tasks, {
+                    id: prevTask.length,
+                    value: task
+                }]);
             }}>
-                <input type="text">
+                <input onChange={(e)=>{
+                    setTask(e.target.value)
+                }}
+                type="text" value={task}>
                 </input>
                 <input type = "submit" className='btn btn-success'>
                 </input>
---
+++ todo/src/Components/Task.js
@@ -0,0 +1,14 @@
+import './style.css'
+import {useState} from 'react'
+
+const Task =()=>{
+    console.log("Hellloo");
+    return(
+        <>
+        <h1>
+            |Hello wo=rld
+        </h1>
+        </>
+    )
+}
+export default Task
